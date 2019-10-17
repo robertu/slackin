08c6adc5776a28f91e11d90bafc104dfc7e203a8
Robert Urbańczyk <robertu@robertu.net>
2019-10-17 21:11:30 +0200

    poprawki

--- setup.py
+++ setup.py
@@ -1,17 +1,32 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="Slack-Integration",
-    version="0.0.1",
-    author="Mateusz Olkesa",
-    description="Kod bota",
+setup(
+    name="Slack Integration Bot",
+    version="0.1.1",
+    author="Mateusz Oleksa",
+    author_email="mail@linacti.software",
+    keywords="slack flask bot",
+    description="An implementation of simple slack integration bot",
     scripts=['scripts/slackin', 'scripts/slackinctl'],
-    packages=['slackin'],
-    package_dir={'slackin': 'src/slackin'},
+    packages=find_packages('src'),
+    package_dir={'':'src'},
     long_description=long_description,
-    long_description_content_type="text/markdown",
     python_requires='>=3.6',
+    install_requires=[
+        'flask',
+        'cairosvg',
+        'pycairo',
+        'slackeventsapi',
+        'slackclient',
+        'pillow',
+        'argparse',
+    ],
+    entry_points={
+        'console_scripts': [
+            'slackin = slackin:main',
+        ],
+    },
 )
--- src/slackin/bot.py
+++ src/slackin/bot.py
@@ -2,32 +2,30 @@
 from flask import Flask, request, make_response, Response
 from slackeventsapi import SlackEventAdapter
 from slack import WebClient
+import re
 
 class Bot(object):
-
-    def __init__(self, slack_token=None, ss=None, host=None, port=None, nazwa_bota=None, app=None, client=None, eventLister=None):
-        assert ss is not None, "Muszę mieć signing secret by żyć"
+    app = None
+    client = None
+    def __init__(self, name=None, slack_token=None, signing_secret=None, host='127.0.0.1', port=8000):
+        assert name is not None, "Please give the bot a name"
+        assert signing_secret is not None, "Muszę mieć signing secret by żyć"
         assert slack_token is not None, "Muszę mieć slack token secret by żyć"
         self.slack_token = slack_token
-        self.ss = ss
         self.host = host
         self.port = port
-        self.nazwa_bota = nazwa_bota
-        self.app = Flask("slack-integra")
+        self.name = name
+        self.app = Flask(name)
         self.client = WebClient(token=slack_token)
-        self.eventLister = SlackEventAdapter(signing_secret=ss, endpoint="/slack/events", server=app)
-        
-    def listern(self):
-        @self.eventLister.on("message")
-        def message(self,event_data):  
-            print ("work") 
+        self.eventListener = SlackEventAdapter(signing_secret=signing_secret, endpoint="/events", server=self.app)
+
+        @self.eventListener.on("message")
+        def message(event_data):
             try:
                 cmd = re.split(r'\W+',event_data["event"]["text"])
             except KeyError:
                 return
             channel = event_data["event"]["channel"]
-            print("Channel:", channel)
-            print(cmd)
             try:
                 nazwa = cmd[0]
                 komenda = cmd[1]
@@ -37,15 +35,15 @@ class Bot(object):
                     args = []
             except IndexError:
                 return make_response("", 200)
-            if not (nazwa in [self.nazwa_bota, 'all']):
+            if not (nazwa in [self.name, 'all']):
                 return make_response("", 200)
-            if komenda == 'witaj':
+            if komenda == 'hello':
                 mess = self.client.chat_postMessage(
-                channel=event_data["event"]["channel"],
-                text='Witaj'
+                    channel=event_data["event"]["channel"],
+                    text='Welcome!'
                 )
+            return make_response("", 200)
 
-    
     def run(self):
         self.app.run(host=self.host, port=self.port, debug=False)
 
