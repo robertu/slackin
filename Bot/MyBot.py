2d9554c524fe8b76466583b12b0dc3013d0b88fb
Mateusz <mcmatie@wp.pl>
2019-10-17 12:07:21 +0200

    Dodanie nasłuchiwania

--- Bot/MyBot.py
+++ Bot/MyBot.py
@@ -4,5 +4,5 @@ class MyBot(Bot):
     def test(self):
         print(self.ss)
     
-bot = MyBot("xoxb-779874283382-788993604672-absKGQndADDnDNwYukXMWnZM","18fef276a83cc2f4d8d08b2ec86c7d77","0.0.0.0", 4004)
+bot = MyBot("xoxb-779874283382-788993604672-absKGQndADDnDNwYukXMWnZM","18fef276a83cc2f4d8d08b2ec86c7d77","0.0.0.0", 4004, "bot4")
 bot.run()
