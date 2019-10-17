e0473f9add8fa4c87684a475a942e9b9d23c495c
Mateusz <mcmatie@wp.pl>
2019-10-17 12:18:49 +0200

    zmiana

--- Bot/MyBot.py
+++
@@ -1,8 +0,0 @@
-from Bot import Bot
-
-class MyBot(Bot):
-    def test(self):
-        print(self.ss)
-    
-bot = MyBot("xoxb-779874283382-788993604672-absKGQndADDnDNwYukXMWnZM","18fef276a83cc2f4d8d08b2ec86c7d77","0.0.0.0", 4004, "bot4")
-bot.run()
